808d08658ffb12be808832e4bd9d7cc324d0a525
William Hou <williamhou38@yahoo.com>
2022-07-30 20:27:53 -0500

    new change

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from '../recipe.model';
     styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+    @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
     recipes: Recipe[] = [
         new Recipe('Test recipe 1', 'This is test recipe 1', 'https://www.eatwell101.com/wp-content/uploads/2020/10/Garlic-Butter-Steak-recipe-roasted-in-Oven.jpg'),
@@ -19,4 +21,7 @@ export class RecipeListComponent implements OnInit {
     ngOnInit(): void {
     }
 
+    onRecipeSelected(recipe: Recipe) {
+        this.recipeWasSelected.emit(recipe);
+    }
 }
