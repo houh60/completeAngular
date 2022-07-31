9db42fe44cb687587268f3cfb3b1848a6464d839
William Hou <williamhou38@yahoo.com>
2022-07-31 16:02:42 -0500

    new change

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
-  selector: 'app-shopping-edit',
-  templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+    selector: 'app-shopping-edit',
+    templateUrl: './shopping-edit.component.html',
+    styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
 
-  constructor() { }
+    @ViewChild('nameInput') nameInput?: ElementRef;
+    @ViewChild('amountInput') amountInput?: ElementRef;
+    @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  ngOnInit(): void {
-  }
+    constructor() {}
 
+    ngOnInit(): void {
+    }
+
+    onAddIngredients() {
+        const newIngredients = new Ingredient(this.nameInput?.nativeElement.value, this.amountInput?.nativeElement.value);
+        this.ingredientAdded.emit(newIngredients);
+    }
+
+    onClear() {
+        (this.nameInput?.nativeElement).value = '';
+        (this.amountInput?.nativeElement).value = '';
+    }
 }
