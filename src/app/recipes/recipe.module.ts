26763e34f3bfd374c143e951218352743bafc51f
William Hou <williamhou38@yahoo.com>
2022-08-10 20:53:55 -0500

    new changes

--- src/app/recipes/recipe.module.ts
+++ src/app/recipes/recipe.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
